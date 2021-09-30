5b15f3d8bbdabc41aca1105ae7864566b7406126
sunita <sunita.savaliya@gmail.com>
2021-09-29 22:48:38 -0500

    Replace ListView with RecyclerView

--- app/src/main/java/com/servicenow/exercise_kotlin/ReviewListActivityKt.kt
+++ app/src/main/java/com/servicenow/exercise_kotlin/ReviewListActivityKt.kt
@@ -1,53 +1,71 @@
 package com.servicenow.exercise_kotlin
 
-import android.app.ListActivity
+
 import android.os.Bundle
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.BaseAdapter
-import android.widget.ImageView
-import android.widget.TextView
 import com.servicenow.coffee.CoffeeShopReviews
 import com.servicenow.coffee.Review
-import com.servicenow.exercise.R
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.*
+import com.servicenow.exercise.databinding.ActivityMainBinding
+import com.servicenow.exercise.databinding.ActivityMainBinding.inflate
+import com.servicenow.exercise.databinding.ReviewItemBinding
 
-class ReviewListActivityKt : ListActivity() {
+class ReviewListActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        listAdapter = ReviewAdapter()
+
+        binding = inflate(layoutInflater)
+        val view = binding.root
+
+        val adapter = ReviewAdapter { review ->
+            startActivity(ReviewDetailActivity.makeIntent(baseContext, review))
+        }
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+        binding.recyclerView.addItemDecoration(DividerItemDecoration(binding.recyclerView.context, DividerItemDecoration.VERTICAL))
+        binding.recyclerView.adapter = adapter
+
+        adapter.submitList(CoffeeShopReviews.list.toMutableList()) //initial reviews data for list.
+
+        setContentView(view)
+    }
+}
+
+class ReviewAdapter(private val clickCallBack : (Review) -> Unit) : ListAdapter<Review, ReviewAdapter.ViewHolder>(ReviewDiffCallback()) {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = ViewHolder(
+        ReviewItemBinding.inflate(
+            LayoutInflater.from(parent.context), parent, false
+        )
+    )
+
+    override fun onBindViewHolder(holder: ReviewAdapter.ViewHolder, position: Int) {
+        holder.bind(getItem(position), clickCallBack)
     }
 
-    inner class ReviewAdapter : BaseAdapter() {
-        override fun getCount(): Int {
-            return CoffeeShopReviews.list.size
-        }
+    class ViewHolder(private val binding: ReviewItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(review: Review, clickCallBack: (Review) -> Unit) {
+            binding.image.setImageResource(Review.getIconResourceFromName(review.name))
+            binding.text1.text = review.name
+            binding.text2.text = review.review
 
-        override fun getItem(position: Int): Any {
-            return CoffeeShopReviews.list[position]
-        }
-
-        override fun getItemId(position: Int): Long {
-            return position.toLong()
-        }
-
-        override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View? {
-            val row: View = convertView ?: LayoutInflater.from(this@ReviewListActivityKt).inflate(
-                R.layout.review_item,
-                parent,
-                false
-            )
-
-            val reviewImage: ImageView = row.findViewById(R.id.image) as ImageView
-            val shopName = row.findViewById(R.id.text1) as TextView
-            val reviewText = row.findViewById(R.id.text2) as TextView
-
-            val review: Review = CoffeeShopReviews.list[position]
-            shopName.text = review.name
-            reviewText.text = review.review
-            reviewImage.setImageResource(Review.getIconResourceFromName(review.name))
-            return row
+            itemView.setOnClickListener { clickCallBack(review) }
         }
     }
+}
+
+class ReviewDiffCallback : DiffUtil.ItemCallback<Review>() {
+    override fun areItemsTheSame(
+        oldItem: Review,
+        newItem: Review
+    ) = oldItem == newItem //use .id if we have later to optimize.
+
+    override fun areContentsTheSame(
+        oldItem: Review,
+        newItem: Review
+    ) = oldItem == newItem
 }
